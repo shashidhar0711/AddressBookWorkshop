8d0ff8c3cd04e39240e8a56d3683ea5f2bf1c563
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-04 18:58:58 +0530

    [sasic] Add. Able To Edit Existing Contact In AddressBook

--- AddressBookWorkshop/AddressBookRepo.cs
+++ AddressBookWorkshop/AddressBookRepo.cs
@@ -43,6 +43,121 @@ namespace AddressBookWorkshop
         }
 
         /// <summary>
+        /// Edits the contact.
+        /// </summary>
+        public void EditContact()
+        {
+            if (addressBookList.Count == 0)
+            {
+                Console.WriteLine("Contact List is Currently empty!!");
+            }
+            else
+            {
+                Console.WriteLine("Enter your First Name");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Enter your Last Name");
+                string lastName = Console.ReadLine();
+
+                /// By Looping Untill Condtions Ends
+                for (int index = 0; index < addressBookList.Count; index++)
+                {
+                    /// Checking If Exist FirstName or not to purpose Edit
+                    if (addressBookList[index].FirstName.Equals(firstName) && addressBookList[index].LastName.Equals(lastName))
+                    {
+                        /// It takes Choice to Edit at Particualar Position
+                        int choice = UserChoiceForEdit();
+                        EditContactList(addressBookList[index], choice);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Does not Exist");
+                    }
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Users the choice for edit.
+        /// </summary>
+        /// <returns></returns>
+        public int UserChoiceForEdit()
+        {
+            Console.WriteLine("1 : Enter to Edit FirstName");
+            Console.WriteLine("2 : Enter to Edit LastName");
+            Console.WriteLine("3 : Enter to Edit Address");
+            Console.WriteLine("4 : Enter to Edit City");
+            Console.WriteLine("5 : Enter to Edit State");
+            Console.WriteLine("6 : Enter to Edit PhoneNumber");
+            Console.WriteLine("7 : Enter to Edit Zip");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            /// It returns the Choice
+            return choice;
+        }
+
+        /// <summary>
+        /// Edits the contact list.
+        /// </summary>
+        /// <param name="contact">The contact.</param>
+        /// <param name="choice">The choice.</param>
+        public void EditContactList(AddressBookModel addressBookModel, int choice)
+        {
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the first name");
+                    string firstName = Console.ReadLine();
+                    addressBookRegex.ValidateFirstName(firstName);
+                    addressBookModel.FirstName = firstName;
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the last name");
+                    string lastName = Console.ReadLine();
+                    addressBookRegex.ValidateLastName(lastName);
+                    addressBookModel.LastName = lastName;
+                    break;
+                case 3:
+                    Console.WriteLine("Enter address");
+                    string address = Console.ReadLine();
+                    addressBookModel.Address = address;
+                    break;
+                case 4:
+                    Console.WriteLine("Enter city");
+                    string city = Console.ReadLine();
+                    addressBookModel.City = city;
+                    break;
+                case 5:
+                    Console.WriteLine("Enter state");
+                    string state = Console.ReadLine();
+                    addressBookModel.State = state;
+                    break;
+                case 6:
+                    Console.WriteLine("Enter Phone Number");
+                    string phoneNumber = Console.ReadLine();
+                    addressBookRegex.ValidatePhoneNumber(phoneNumber);
+                    addressBookModel.PhoneNumber = phoneNumber;
+                    break;
+                case 7:
+                    Console.WriteLine("Enter Zip code");
+                    string zipCode = Console.ReadLine();
+                    addressBookRegex.ValidateZipCode(zipCode);
+                    addressBookModel.ZipCode = zipCode;
+                    break;
+                case 8:
+                    Console.WriteLine("Enter Email Id");
+                    string eMailId = Console.ReadLine();
+                    addressBookRegex.ValidateEmailId(eMailId);
+                    addressBookModel.EmailID = eMailId;
+                    break;
+                default:
+                    Console.WriteLine("Enter valid choice");
+                    break;
+            }
+        }
+
+        /// <summary>
         /// Displays the contact.
         /// </summary>
         public void DisplayContact()
@@ -65,7 +180,7 @@ namespace AddressBookWorkshop
             }
             else
             {
-                Console.WriteLine("No contact to display");
+                Console.WriteLine("No contact to display!!");
             }
         }
     }
